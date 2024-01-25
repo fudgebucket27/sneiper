1c58fb1a2facb9be7f3e9cc19489882cbe856554
Fudgey <fudgebucket27@gmail.com>
2024-01-25 17:13:00 +1000

    more boiler plate

--- sneiper.js
+++ sneiper.js
@@ -2,11 +2,20 @@ import {configDotenv} from "dotenv";
 
 import { restoreWallet } from "@sei-js/core";
 
+import { calculateFee } from '@cosmjs/stargate';
+
+import { getSigningCosmWasmClient } from "@sei-js/core";
+
 async function main() {
-    configDotenv.apply();
-    const restoredWallet = await restoreWallet(process.env.RECOVERY_PHRASE);
-    const accounts = await restoredWallet.getAccounts();
-    console.log(accounts[0].address);
+    configDotenv.apply(); //Get .env file 
+    const restoredWallet = await restoreWallet(process.env.RECOVERY_PHRASE); //restore wallet from .env RECOVERY_PHRASE
+    const accounts = await restoredWallet.getAccounts(); //get accounts
+    const senderAddress = accounts[0].address; //get address
+
+    const fee = calculateFee(600000, "0.6") //this is the gas limit
+
+    const result = await restoredWallet.getSigningCosmWasmClient(process.env.RPC_URL);
+
 }
 
 main();

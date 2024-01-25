62891bceb6facf3b2dc9918ea8c3884a6bc22306
John <fudgebucket27@gmail.com>
2024-01-26 00:05:52 +1000

    Update sneiper.js
    catch error

--- sneiper.js
+++ sneiper.js
@@ -12,20 +12,24 @@ async function main() {
 
     const fee = calculateFee(600000, "0.6usei") //this is the gas limit
     const msg =  {
-        "buy_now" : {
-          "expected_price": {
-            "amount": "660000",
-            "denom": "usei"
-          },
-          "nft": {
-            "address": "sei1faqw953tzda4qvr37wnjpr99t0hx6nqeknrf9c03gzrjvm764huqgvspst",
-            "token_id": "1804"
-          }
+        "buy_now": {
+            "expected_price": {
+                "amount": "40000000",
+                "denom": "usei"
+            },
+            "nft": {
+                "address": "sei1faqw953tzda4qvr37wnjpr99t0hx6nqeknrf9c03gzrjvm764huqgvspst",
+                "token_id": "2850"
+            }
         }
-      };
+    };
 
     const signingCosmWasmClient = await getSigningCosmWasmClient(process.env.RPC_URL, restoredWallet);
-    const result = await signingCosmWasmClient.execute(senderAddress, "sei152u2u0lqc27428cuf8dx48k8saua74m6nql5kgvsu4rfeqm547rsnhy4y9", msg, fee);
+    try {
+      const result = await signingCosmWasmClient.execute(senderAddress, "sei152u2u0lqc27428cuf8dx48k8saua74m6nql5kgvsu4rfeqm547rsnhy4y9", msg, fee);
+    } catch (error){
+        console.log(error.message);
+    }
 }
 
 main();

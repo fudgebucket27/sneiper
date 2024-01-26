06dbb5f92ad5d278ee03124bf1813c90dd407b80
Fudgey <fudgebucket27@gmail.com>
2024-01-27 05:42:08 +1000

    more logic done

--- sneiper.js
+++ sneiper.js
@@ -8,27 +8,67 @@ async function main() {
     const accounts = await restoredWallet.getAccounts(); //get accounts
     const senderAddress = accounts[0].address; //get address
 
-    const msg =  {
+    try {
+      console.log("Sneiper executing...");
+
+      //Getting data from pallet API
+      const palletListingResponse = await fetch("https://api.prod.pallet.exchange/api/v1/nfts/"+ process.env.CONTRACT_ADDRESS + "?get_tokens=true&token_id=" + process.env.TOKEN_ID + "&token_id_exact=true");
+      if (!palletListingResponse.ok) {
+        let errorMsg = "";
+        try {
+            const errorData = await palletListingResponse.json();
+            errorMsg = errorData.message || JSON.stringify(errorData); 
+        } catch (parseError) {
+            errorMsg = palletListingResponse.statusText;
+        }
+        throw new Error(`Failed to get pallet listing! ${errorMsg}`);
+      }
+
+      const palletListingResponseData = await palletListingResponse.json();
+
+      //Executing the pallet smart contract
+      if(palletListingResponseData.tokens[0].auction == null)
+      {
+        throw new Error("There is no listing for this NFT!");
+      }
+
+      if(palletListingResponseData.tokens[0].auction.type !== "fixed_price")
+      {
+        throw new Error("The listing for this NFT is not buy now!");
+      }
+
+      if(palletListingResponseData.tokens[0].auction.price_float <= process.env.PRICE_LIMIT)
+      {
+        
+      }
+      else
+      {
+        throw new Error("The listing exceeds the PRICE_LIMIT!");
+      }
+
+      const msg =  {
         "buy_now": {
             "expected_price": {
-                "amount": "400000",
+                "amount": palletListingResponseData.tokens[0].auction.price[0].amount,
                 "denom": "usei"
             },
             "nft": {
-                "address": "sei1faqw953tzda4qvr37wnjpr99t0hx6nqeknrf9c03gzrjvm764huqgvspst",
-                "token_id": "3490"
+                "address": process.env.CONTRACT_ADDRESS,
+                "token_id": process.env.TOKEN_ID
             }
         }
     };
 
-    const totalFunds = [{
-      denom: 'usei',
-      amount: "408000" //Add 2% for pallet fee
-    }];
+      const amountString = palletListingResponseData.tokens[0].auction.price[0].amount;
+      const amountNumber = parseFloat(amountString);
+      const finalPalletAmount = amountNumber + (amountNumber * 0.02); //Add 2% for pallet fee
 
-    const signingCosmWasmClient = await getSigningCosmWasmClient(process.env.RPC_URL, restoredWallet, {gasPrice: "0.1usei"});
-    try {
-      console.log("Sneiper executing...");
+      const totalFunds = [{
+        denom: 'usei',
+        amount: finalPalletAmount.toString()
+      }];
+      
+      const signingCosmWasmClient = await getSigningCosmWasmClient(process.env.RPC_URL, restoredWallet, {gasPrice: process.env.GAS_LIMIT + "usei"});
       const result = await signingCosmWasmClient.execute(senderAddress, "sei152u2u0lqc27428cuf8dx48k8saua74m6nql5kgvsu4rfeqm547rsnhy4y9", msg, "auto", "sneiper", totalFunds );
       if(result.transactionHash){
         console.log("Sneipe successful! Tx hash: " + result.transactionHash);

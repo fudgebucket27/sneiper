ec0ec5f772716f2c91628bb0f6b00b74dffe97e4
Fudgey <fudgebucket27@gmail.com>
2024-01-27 06:04:41 +1000

    update polling logic

--- sneiper.js
+++ sneiper.js
@@ -2,7 +2,10 @@ import {configDotenv} from "dotenv";
 import { restoreWallet,  getSigningCosmWasmClient} from "@sei-js/core";
 configDotenv.apply(); //Get .env file 
 
-async function sneiper() {
+const pollingIntervalIds = []; // Array to hold all polling interval IDs
+
+
+async function sneiper(senderAddress) {
     try {
       console.log("Sneiper executing...");
       const restoredWallet = await restoreWallet(process.env.RECOVERY_PHRASE); //restore wallet from .env RECOVERY_PHRASE
@@ -65,6 +68,8 @@ async function sneiper() {
       const result = await signingCosmWasmClient.execute(senderAddress, "sei152u2u0lqc27428cuf8dx48k8saua74m6nql5kgvsu4rfeqm547rsnhy4y9", msg, "auto", "sneiper", totalFunds );
       if(result.transactionHash){
         console.log("Sneipe successful! Tx hash: " + result.transactionHash);
+        clearAllIntervals(); 
+        process.exit(0);
       }
       else {
         console.log("Sneipe unsuccessful")
@@ -83,13 +88,18 @@ async function main() {
         //Run sneiper based on polling time
         const pollingFrequency = parseInt(process.env.POLLING_FREQUENCY, 10) * 1000;
         if (!isNaN(pollingFrequency) && pollingFrequency > 0) {
-          setInterval(() => sneiper(senderAddress), pollingFrequency); // Run executePeriodically at intervals
+          const intervalId = setInterval(() => sneiper(senderAddress), pollingFrequency);
+          pollingIntervalIds.push(intervalId);
         } else {
             console.error("Invalid POLLING_FREQUENCY. Please set a valid number in seconds");
         }
     } catch (error) {
       console.error("Error initializing wallet: " + error.message);
   }
+}
+
+function clearAllIntervals() {
+  pollingIntervalIds.forEach((id) => clearInterval(id));
 }
 
 main();

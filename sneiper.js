94246288188af8fdd562784889c3f7db535decd1
Fudgey <fudgebucket27@gmail.com>
2024-01-27 10:45:22 +1000

    batch buying working

--- sneiper.js
+++ sneiper.js
@@ -9,23 +9,43 @@ let isProcessingQueue = false;
 async function sneiper(senderAddress, restoredWallet) {
     try {
       //Getting data from pallet API
-      const palletListingResponse = await fetch("https://api.prod.pallet.exchange/api/v1/nfts/"+ process.env.CONTRACT_ADDRESS + "?get_tokens=true&token_id=" + process.env.TOKEN_ID + "&token_id_exact=true");
-      if (!palletListingResponse.ok) {
-        let errorMsg = "";
-        try {
-            const errorData = await palletListingResponse.json();
-            errorMsg = errorData.message || JSON.stringify(errorData); 
-        } catch (parseError) {
-            errorMsg = palletListingResponse.statusText;
+      if(process.env.TOKEN_ID === "ALL") {
+        const palletListingResponse = await fetch("https://api.prod.pallet.exchange/api/v2/nfts/" + process.env.CONTRACT_ADDRESS +"?get_tokens=true&token_id_exact=false&buy_now_only=true&min_price_only=false&not_for_sale=false&less_than_price=" + process.env.PRICE_LIMIT + "&sort_by_price=asc&sort_by_id=asc&page=1&page_size=25");
+        if (!palletListingResponse.ok) {
+          let errorMsg = "";
+          try {
+              const errorData = await palletListingResponse.json();
+              errorMsg = errorData.message || JSON.stringify(errorData); 
+          } catch (parseError) {
+              errorMsg = palletListingResponse.statusText;
+          }
+          throw new Error(`Failed to get pallet listings! ${errorMsg}`);
         }
-        throw new Error(`Failed to get pallet listing! ${errorMsg}`);
-      }
-      const palletListingResponseData = await palletListingResponse.json();
-
-      if (isValidListing(palletListingResponseData) && !isProcessingQueue) {
-        console.log("Listing valid! Sneiping...")
-        executionQueue.push({ senderAddress, palletListingResponseData, restoredWallet });
-        processQueue();
+        const palletListingResponseData = await palletListingResponse.json();
+        if (palletListingResponseData.count > 0 && !isProcessingQueue) {
+          console.log("Listings valid! Sneiping...")
+          executionQueue.push({ senderAddress, palletListingResponseData, restoredWallet });
+          processQueue();
+        }
+      }else {
+        const palletListingResponse = await fetch("https://api.prod.pallet.exchange/api/v1/nfts/"+ process.env.CONTRACT_ADDRESS + "?get_tokens=true&token_id=" + process.env.TOKEN_ID + "&token_id_exact=true");
+        if (!palletListingResponse.ok) {
+          let errorMsg = "";
+          try {
+              const errorData = await palletListingResponse.json();
+              errorMsg = errorData.message || JSON.stringify(errorData); 
+          } catch (parseError) {
+              errorMsg = palletListingResponse.statusText;
+          }
+          throw new Error(`Failed to get pallet listing! ${errorMsg}`);
+        }
+        const palletListingResponseData = await palletListingResponse.json();
+  
+        if (isValidListing(palletListingResponseData) && !isProcessingQueue) {
+          console.log("Listing valid! Sneiping...")
+          executionQueue.push({ senderAddress, palletListingResponseData, restoredWallet });
+          processQueue();
+        }
       }
     } catch (error){
         console.log("Sneipe unsuccessful! " + error.message);
@@ -52,7 +72,12 @@ async function processQueue() {
   const { senderAddress, palletListingResponseData, restoredWallet } = executionQueue.shift();
 
   try {
+    if(process.env.TOKEN_ID === "ALL"){
+      await executeContractMultiple(senderAddress, palletListingResponseData, restoredWallet);
+    }
+    else{
       await executeContract(senderAddress, palletListingResponseData, restoredWallet);
+    }
   } catch (error) {
       console.log("Sneipe unsuccessful! " + error.message);
   } finally {
@@ -98,6 +123,57 @@ async function executeContract(senderAddress, palletListingResponseData, restore
     } catch (error) {
       console.log("Sneipe unsuccessful! " + error.message);
     }
+}
+
+async function executeContractMultiple(senderAddress, palletListingResponseData, restoredWallet) {
+  try {
+
+    let batchBids = {
+      "batch_bids": {
+          "bids": []
+      }
+    };
+    
+    const batchBidsSliced = palletListingResponseData.tokens.slice(0, process.env.BUY_LIMIT).map(token => ({
+      "bid_type": {
+        "buy_now": {
+          "expected_price": {
+            "amount": token.auction.price[0].amount.toString(),
+            "denom": "usei"
+          }
+        }
+      },
+      "nft": {
+        "address": process.env.CONTRACT_ADDRESS,
+        "token_id": token.id_int.toString()
+      }
+    }));
+
+      batchBids.batch_bids.bids = batchBidsSliced;
+
+      let totalAmount = 0;
+      batchBidsSliced.forEach(bid => {
+        let amount = parseFloat(bid.bid_type.buy_now.expected_price.amount);
+        totalAmount += amount + (amount * 0.02); // Add amount with 2% fee to total
+      });
+      const totalFunds = [{
+          denom: 'usei',
+          amount: totalAmount.toString()
+      }];
+      const signingCosmWasmClient = await getSigningCosmWasmClient(process.env.RPC_URL, restoredWallet, { gasPrice: process.env.GAS_LIMIT + "usei" });
+
+      const result = await signingCosmWasmClient.execute(senderAddress, "sei152u2u0lqc27428cuf8dx48k8saua74m6nql5kgvsu4rfeqm547rsnhy4y9", batchBids, "auto", "sneiper", totalFunds);
+
+      if (result.transactionHash) {
+          console.log("Sneipe successful! Tx hash: " + result.transactionHash);
+          clearAllIntervals();
+          process.exit(0);
+      } else {
+          console.log("Sneipe unsuccessful!");
+      }
+  } catch (error) {
+      console.log("Sneipe unsuccessful! " + error.message);
+  }
 }
 
 async function main() {

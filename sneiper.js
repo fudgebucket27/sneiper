0295451333a2d1b5afd9c857ae0fd7c18508c582
Fudgey <fudgebucket27@gmail.com>
2024-01-28 21:03:59 +1000

    add auto and sweep flags

--- sneiper.js
+++ sneiper.js
@@ -1,3 +1,4 @@
+//I'm not a javascript dev. Don't judge me. I'm trying lmao.
 import {configDotenv} from "dotenv";
 import { restoreWallet,  getSigningCosmWasmClient} from "@sei-js/core";
 configDotenv.apply(); //Get .env file 
@@ -6,8 +7,7 @@ const pollingIntervalIds = [];
 let executionQueue = [];
 let isProcessingQueue = false;
 let targetTokenIds;
-if(process.env.TOKEN_ID !== "ALL")
-{
+if (process.env.TOKEN_ID !== "SWEEP" && process.env.TOKEN_ID !== "AUTO"){
   targetTokenIds = new Set(process.env.TOKEN_ID.split(',').map(id => parseInt(id.trim(), 10)));
 }
 const boughtTokenIds = new Set(); 
@@ -15,7 +15,7 @@ const boughtTokenIds = new Set();
 async function sneiper(senderAddress, restoredWallet) {
     try {
       //Getting data from pallet API
-      if(process.env.TOKEN_ID === "ALL") {
+      if(process.env.TOKEN_ID === "SWEEP") {
         const palletListingResponse = await fetch("https://api.prod.pallet.exchange/api/v2/nfts/" + process.env.CONTRACT_ADDRESS +"?get_tokens=true&token_id_exact=false&buy_now_only=true&min_price_only=false&not_for_sale=false&less_than_price=" + process.env.PRICE_LIMIT + "&sort_by_price=asc&sort_by_id=asc&page=1&page_size=25");
         if (!palletListingResponse.ok) {
           let errorMsg = "";
@@ -33,7 +33,25 @@ async function sneiper(senderAddress, restoredWallet) {
           executionQueue.push({ senderAddress, palletListingResponseData, restoredWallet });
           processQueue();
         }
-      }else {
+      } else if(process.env.TOKEN_ID === "AUTO") {
+        const palletListingResponse = await fetch("https://api.prod.pallet.exchange/api/v2/nfts/" + process.env.CONTRACT_ADDRESS +"?get_tokens=true&token_id_exact=false&buy_now_only=true&min_price_only=false&not_for_sale=false&less_than_price=" + process.env.PRICE_LIMIT + "&sort_by_price=asc&sort_by_id=asc&page=1&page_size=25");
+        if (!palletListingResponse.ok) {
+          let errorMsg = "";
+          try {
+              const errorData = await palletListingResponse.json();
+              errorMsg = errorData.message || JSON.stringify(errorData); 
+          } catch (parseError) {
+              errorMsg = palletListingResponse.statusText;
+          }
+          throw new Error(`Failed to get pallet listings! ${errorMsg}`);
+        }
+        const palletListingResponseData = await palletListingResponse.json();
+        if (palletListingResponseData.count > 0 && !isProcessingQueue) {
+          console.log("Listing valid! Sneiping...")
+          executionQueue.push({ senderAddress, palletListingResponseData, restoredWallet });
+          processQueue();
+        }
+      } else {
         const tokenIds = process.env.TOKEN_ID.split(',').map(id => parseInt(id.trim(), 10));
 
         for (const tokenId of tokenIds) {
@@ -86,8 +104,11 @@ async function processQueue() {
   const { senderAddress, palletListingResponseData, restoredWallet } = executionQueue.shift();
 
   try {
-    if(process.env.TOKEN_ID === "ALL"){
+    if(process.env.TOKEN_ID === "SWEEP"){
       await executeContractMultiple(senderAddress, palletListingResponseData, restoredWallet);
+    }
+    if(process.env.TOKEN_ID === "AUTO"){
+      await executeContractAuto(senderAddress, palletListingResponseData, restoredWallet);
     }
     else{
       await executeContract(senderAddress, palletListingResponseData, restoredWallet);
@@ -130,7 +151,7 @@ async function executeContract(senderAddress, palletListingResponseData, restore
         boughtTokenIds.add(palletListingResponseData.tokens[0].id_int);
         console.log("Sneipe successful for token id:" + palletListingResponseData.tokens[0].id_int + ", Tx hash: " + result.transactionHash);
     
-        if (boughtTokenIds.size === targetTokenIds.size) {
+        if (boughtTokenIds.size === targetTokenIds.size || boughtTokenIds.size ===  process.env.BUY_LIMIT ) {
             console.log("All tokens have been successfully bought. Exiting...");
             clearAllIntervals();
             process.exit(0);
@@ -196,6 +217,56 @@ async function executeContractMultiple(senderAddress, palletListingResponseData,
   }
 }
 
+async function executeContractAuto(senderAddress, palletListingResponseData, restoredWallet) {
+  for (const token of palletListingResponseData.tokens) {
+    try {
+      const bid = {
+        "buy_now": {
+          "expected_price": {
+            "amount": token.auction.price[0].amount.toString(),
+            "denom": "usei"
+          },
+          "nft": {
+            "address": process.env.CONTRACT_ADDRESS,
+            "token_id": token.id.toString() 
+          }
+        }
+      };
+
+      const amountNumber = parseFloat(token.auction.price[0].amount);
+      const finalAmount = amountNumber + (amountNumber * 0.02); // 2% fee
+
+      const totalFunds = [{
+          denom: 'usei',
+          amount: finalAmount.toString()
+      }];
+
+      const signingCosmWasmClient = await getSigningCosmWasmClient(process.env.RPC_URL, restoredWallet, { gasPrice: process.env.GAS_LIMIT + "usei" });
+
+      const result = await signingCosmWasmClient.execute(senderAddress, "sei152u2u0lqc27428cuf8dx48k8saua74m6nql5kgvsu4rfeqm547rsnhy4y9", bid, "auto", "sneiper", totalFunds);
+
+      if (result.transactionHash) {
+          boughtTokenIds.add(token.id_int);
+          const buyLimit = parseInt(process.env.BUY_LIMIT, 10); 
+          console.log("Sneipe successful for token id:" + token.id_int + ", Tx hash: " + result.transactionHash);
+          if (boughtTokenIds.size ===  buyLimit) {
+              console.log("All tokens have been successfully bought. Exiting...");
+              clearAllIntervals();
+              process.exit(0);
+          }
+      } else {
+          console.log(`Sneipe unsuccessful for token id: ${token.id}`);
+      }
+    } catch (error) {
+      console.log("Sneipe unsuccessful! " + error.message);
+    }
+  }
+
+  console.log("All tokens have been processed. Exiting...");
+  clearAllIntervals();
+  process.exit(0);
+}
+
 async function main() {
   try {
       console.log("Sneiper watching listings...");
@@ -220,4 +291,4 @@ function clearAllIntervals() {
   pollingIntervalIds.forEach((id) => clearInterval(id));
 }
 
-main();
+main()
